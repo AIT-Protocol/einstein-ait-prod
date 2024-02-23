2a527fd94bf4e4e280c19f95d0a7583288f724d0
ndxbinh1922001 <binhndx1922001@gmail.com>
2024-02-23 22:32:40 +0700

    fix bug rewards == null

--- einstein/rewards/reward.py
+++ einstein/rewards/reward.py
@@ -123,8 +123,9 @@ class BatchRewardOutput:
     def __post_init__(self):
         if self.rewards.shape != self.timings.shape:
             raise ValueError(f"rewards.shape {self.rewards.shape} != timings.shape {self.timings.shape}")
-        
-        self.rewards_normalized = (self.rewards-self.rewards.min())/(self.rewards.max()-self.rewards.min()+1e-6)
+        # if rewards == null skip normalization
+        if self.rewards.numel() != 0:
+            self.rewards_normalized = (self.rewards-self.rewards.min())/(self.rewards.max()-self.rewards.min()+1e-6)
 
 
 class BaseRewardModel(ABC):
