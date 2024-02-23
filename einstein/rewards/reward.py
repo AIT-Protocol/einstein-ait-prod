60fb6c3d71eaafe7f77b3ec2a766ac783be4ec82
ndxbinh1922001 <binhndx1922001@gmail.com>
2024-02-23 23:29:58 +0700

    fix rewards normalized

--- einstein/rewards/reward.py
+++ einstein/rewards/reward.py
@@ -123,8 +123,9 @@ class BatchRewardOutput:
     def __post_init__(self):
         if self.rewards.shape != self.timings.shape:
             raise ValueError(f"rewards.shape {self.rewards.shape} != timings.shape {self.timings.shape}")
-        # if rewards == null skip normalization
-        if self.rewards.numel() != 0:
+        if self.rewards.numel() == 0:
+             self.rewards_normalized = torch.zeros_like(self.rewards)
+        else:
             self.rewards_normalized = (self.rewards-self.rewards.min())/(self.rewards.max()-self.rewards.min()+1e-6)
 
 
