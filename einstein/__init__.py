692ac19a8f10553e9d85970219143ff0df126509
Tony <tony.leviethung@gmail.com>
2024-06-24 13:24:21 -0700

    change version number

--- einstein/__init__.py
+++ einstein/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 version_split = __version__.split(".")
 __spec_version__ = (
     (10000 * int(version_split[0]))
@@ -18,6 +18,7 @@ from . import forward
 from . import agent
 from . import conversation
 from . import dendrite
+
 # from . import shared
 from . import validator
 
